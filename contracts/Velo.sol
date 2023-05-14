527680699b7b896edfe647589bb7937958711568
CrazyNorman <cryptopunk1982@gmail.com>
2023-05-14 21:56:27 +0800

    edit token and nft name

--- contracts/Velo.sol
+++ contracts/Velo.sol
@@ -5,8 +5,8 @@ import "contracts/interfaces/IVelo.sol";
 
 contract Velo is IVelo {
 
-    string public constant name = "Velodrome";
-    string public constant symbol = "VELO";
+    string public constant name = "veSync";
+    string public constant symbol = "VS";
     uint8 public constant decimals = 18;
     uint public totalSupply = 0;
 
