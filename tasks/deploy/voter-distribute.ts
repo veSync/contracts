912bcf9ce404cb342aa10ee74a9df31e103b306a
yakifugu <cyberpenguin2@protonmail.com>
2023-06-01 10:52:39 -0400

    improve voter-distribute script

--- tasks/deploy/voter-distribute.ts
+++ tasks/deploy/voter-distribute.ts
@@ -4,6 +4,7 @@ import { task } from "hardhat/config";
 // npx hardhat deploy:distribute --network zkMain
 
 const VOTER_ADDRESS = "0xca9c5032D9C72A5028cC760Fd0Cbb45798e68705"; // mainnet
+const VS_ADDRESS = "0x5756A28E2aAe01F600FC2C01358395F5C1f8ad3A";
 
 // skip major gauges
 const SKIP_GAUGE = ["0xd6753a142C9fd1ac6281D738BaD0e2cFf9C91c4B", "0x3af4678f3dcE1051EF182f222979D25920C6e342", "0x0a35447c43D766BDa5C41d32AF2376ECA8BDaDa5"].map(x => x.toLowerCase());
@@ -28,6 +29,14 @@ async function deploy(taskArgs: any) {
       console.log(`id: ${i}, pool: ${pool}, gauge: ${gauge}, is not alive. Skipping...`);
       continue;
     }
+
+    const gaugeContract = await hre.ethers.getContractAt("Gauge", gauge);
+    const periodFinish = await gaugeContract.periodFinish(VS_ADDRESS);
+    if (periodFinish > Date.now() / 1000) {
+      console.log(`id: ${i}, pool: ${pool}, gauge: ${gauge}, periodFinish: ${periodFinish}, is not finished. Skipping...`);
+      continue;
+    }
+
     console.log("sending tx...")
     try {
       await c['distribute(uint256,uint256)'](i, i + 1);
